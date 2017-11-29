57206e21445cd5e3d65d15ba953b2b3b8d85bf7f
Don Mohlmaster <dmohlmaster@tervis.com>
2017-11-29 14:29:54 -0500

    +Invoke-CreateOracleLinuxRDMSessions

---
+++ TervisRemoteDesktopManager.psm1
@@ -0,0 +1,15 @@
+﻿function Invoke-CreateOracleLinuxRDMSessions {
+    $OracleHosts = Get-TervisHostGroupCNAMEAndA -HostGroupName Oracle
+    
+    #$Hostname = "EBSDB-PRD"
+    #$Environment = "Production"
+    
+    $RDMTemplate = Get-RDMTemplate | where name -eq "Linux Standard"
+    $OracleHosts | %{
+        $RDMSession = New-RDMSession -TemplateID $RDMTemplate.ID -Host $_.HostName -Name $_.HostName -Type putty -Group $_.EnvironmentName
+        Set-RDMSession -Session $RDMSession 
+        Update-RDMUI
+        Set-RDMSessionProperty -ID $RDMSession.ID -Path MetaInformation -Property OS -Value Linux
+    }
+}
+
